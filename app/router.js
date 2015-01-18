3511a79de9030c21af7ae790ada94e6719f9eed7
dfreire <dario.freire@gmail.com>
2015-01-18 00:02:39 +0000

    update

--- app/router.js
+++ app/router.js
@@ -2,10 +2,22 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+	location: config.locationType
 });
 
 Router.map(function() {
+	this.resource('sign-in', {
+		path: '/'
+	});
+	this.resource('sign-in', {
+		path: '/sign-in'
+	});
+	this.resource('sign-up', {
+		path: '/sign-up'
+	});
+	this.resource('reset-password', {
+		path: '/reset-password'
+	});
 });
 
 export default Router;
