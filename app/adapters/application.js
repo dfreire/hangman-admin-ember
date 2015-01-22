9cf3ce2ab5bf48e115119cbf1f657bfde6b7275c
dfreire <dario.freire@gmail.com>
2015-01-22 21:50:29 +0000

    update

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,8 @@
-import DS from 'ember-data';
+import DS from "ember-data";
 
-export default DS.RESTAdapter.reopen({
-	namespace: 'api',
-    host: 'http://localhost:9100'
+var ApplicationAdapter = DS.RESTAdapter.extend({
+    host: 'localhost:9100',
+    namespace: 'api/v1'
 });
+
+export default ApplicationAdapter;
